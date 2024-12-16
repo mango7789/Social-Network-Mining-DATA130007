8d13828829dfdda21320cee8298ddd0a2106afb2
mango7789 <981653507@qq.com>
2024-12-16 16:43:19 +0800

    Rename community directory, move d3.js to visualization folder

--- community-mining/louvain.py
+++ community-mining/louvain.py
@@ -3,13 +3,13 @@ from typing import Dict
 import networkx as nx
 from community import community_louvain
 import os
-from tqdm import tqdm 
+from tqdm import tqdm
 
 
 def louvain(node: pd.DataFrame, edge: pd.DataFrame, **kwargs: Dict) -> None:
     """
     Perform community detection on the given node and edge data. The nodes and edges are expected
-    to be provided as DataFrames, which are generated after pre-processing. `kwargs` can include 
+    to be provided as DataFrames, which are generated after pre-processing. `kwargs` can include
     additional hyperparameters to fine-tune the algorithm.
 
     This function should also assign a community label to each node and save these labels in a static
@@ -18,23 +18,23 @@ def louvain(node: pd.DataFrame, edge: pd.DataFrame, **kwargs: Dict) -> None:
     NOTE: For large graphs, it is recommended to use `igraph` over `networkx` for improved performance.
 
     >>> # Example usage
-    >>> louvain(paper_node, paper_edge) 
+    >>> louvain(paper_node, paper_edge)
     """
     G = nx.DiGraph()
 
     for index in tqdm(node.index, total=len(node), desc="Adding nodes to graph"):
         row = node.loc[index]
-        G.add_node(row['id'])
+        G.add_node(row["id"])
 
     for index in tqdm(edge.index, total=len(edge), desc="Adding edges to graph"):
         row = edge.loc[index]
-        G.add_edge(row['src'], row['dst'])
+        G.add_edge(row["src"], row["dst"])
 
     partition = community_louvain.best_partition(G.to_undirected())
 
-    node['community'] = node['id'].map(partition)
+    node["community"] = node["id"].map(partition)
 
-    result_path = os.path.join(os.getcwd(), 'results', 'node_with_community.csv')
+    result_path = os.path.join(os.getcwd(), "results", "node_with_community.csv")
 
     os.makedirs(os.path.dirname(result_path), exist_ok=True)
 
@@ -43,9 +43,7 @@ def louvain(node: pd.DataFrame, edge: pd.DataFrame, **kwargs: Dict) -> None:
     print("Community detection completed and results saved.")
 
 
-'''
-# Example usage
-node_data = pd.read_csv('../data/paper/node.csv', sep=',', low_memory=False)
-edge_data = pd.read_csv('../data/paper/edge.csv', sep=',', low_memory=False)
-louvain(node_data, edge_data)
-'''
+if __name__ == "__main__":
+    node_data = pd.read_csv("../test/paper/node.csv", sep=",", low_memory=False)
+    edge_data = pd.read_csv("../test/paper/edge.csv", sep=",", low_memory=False)
+    louvain(node_data, edge_data)
